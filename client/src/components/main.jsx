f1dd17d55e92eed7a5929c2548a9d54d06e73608
Ville Toikkanen <vtoikkanen@hotmail.fi>
2019-06-02 17:59:53 +0300

    komponentteja lisätty fronttiin
    - main, register ja login-komponentit lisätty
    - tyylejä lisätty

---
+++ client/src/components/main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Register from './register';
+import '../styles/main.css';
+import "typeface-open-sans";
+
+class Main extends Component {
+    render(){
+        return(
+            <div>
+                <Register />
+            </div>
+        );
+    }
+}
+
+export default Main;
---
+++ client/src/components/register.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/register.css';
+
+class Register extends Component {
+    render(){
+        return(
+            <div className="register">
+                <h1>Register</h1>
+            </div>
+        );
+    }
+}
+
+export default Register;
