3782f95e67a49ee53d303825167d10702a428ffc
Paavo Ronni <paavo.ronni@student.tut.fi>
2019-06-02 15:46:48 +0300

    Login lisätty

--- routes/api/user.js
+++ routes/api/user.js
@@ -62,4 +62,52 @@ router.post('/register',[
     }
 });
 
+// @route   POST api/user/login
+// @desc    login a user
+// @access  Public
+router.post('/login',[
+    check('email', 'Please include valid email').isEmail(),
+    check('password','Password is required')
+        .exists()
+], async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        const {email, password} = req.body;
+        let user = await User.findOne({email:email});
+        
+        if (!user) {
+            return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({errors: [{msg: 'Invalid credentials'}]});
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err,token)=>{
+                if(err) throw err;
+                res.json({token});
+            });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error!');
+    }
+
+});
+
 module.exports = router;
