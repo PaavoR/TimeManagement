c79bf35ea8afa55f598c4ebd0170bc619d7f01a7
Ville Toikkanen <vtoikkanen@hotmail.fi>
2019-06-02 19:01:14 +0300

    ilmoitus-komponentti lisätty

---
+++ client/src/components/notification.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/notification.css';
+
+class Notification extends Component {
+    render(){
+        return(
+            <div className="notification success">Rekisteröiminen onnistui</div>
+        );
+    }
+}
+
+export default Notification;
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import '../styles/register.css';
 
 class Register extends Component {
+    getSuccess(){
+        return true;
+    }
+
     render(){
         return(
             <div className="register">
                 <h1>Rekisteröidy</h1>
                 <form>
-                    <input type="text" placeholder="Sähköpostiosoite" />
-                    <input type="text" placeholder="Nimi" />
-                    <input type="password" placeholder="Salasana" />
-                    <input type="password" placeholder="Salasana uudelleen" />
-                    <input type="submit" value="Rekisteröidy" />
+                    <input type="text" placeholder="Sähköpostiosoite" className="text" />
+                    <input type="text" placeholder="Nimi" className="text" />
+                    <input type="password" placeholder="Salasana" className="text" />
+                    <input type="password" placeholder="Salasana uudelleen" className="text" />
+                    <input type="button" value="Rekisteröidy" className="submit" onClick={() => this.props.onRegister(this.getSuccess())}/>
                 </form>
             </div>
         );
