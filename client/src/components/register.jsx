fee6da679659354e900543df804bb4fe47483d64
Ville Toikkanen <vtoikkanen@hotmail.fi>
2019-06-02 18:36:26 +0300

    rekisteröitymislomake lisätty

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -5,7 +5,14 @@ class Register extends Component {
     render(){
         return(
             <div className="register">
-                <h1>Register</h1>
+                <h1>Rekisteröidy</h1>
+                <form>
+                    <input type="text" placeholder="Sähköpostiosoite" />
+                    <input type="text" placeholder="Nimi" />
+                    <input type="password" placeholder="Salasana" />
+                    <input type="password" placeholder="Salasana uudelleen" />
+                    <input type="submit" value="Rekisteröidy" />
+                </form>
             </div>
         );
     }
