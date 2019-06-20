66edbaf5d8bdeb12cec476ecf2ecb795093b4114
Paavo Ronni <paavo.ronni@student.tut.fi>
2019-06-20 09:54:57 +0300

    getUserTasks lisätty

--- client/src/services/TaskService.js
+++ client/src/services/TaskService.js
@@ -25,4 +25,49 @@ export default class TaskService {
       }
     }
   }
+  async getUserTasks() {
+    try {
+      const token = getFromStorage("token");
+      const config = {
+        headers: {
+          "x-auth-token": token
+        }
+      };
+      const res = await axios.get("/api/tasks", config);
+      const { data } = res;
+      return data;
+    } catch (err) {
+      if (err.response) {
+        const errors = await err.response.data;
+        console.log(errors);
+        throw errors;
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  async addNewTask(taskData) {
+    try {
+      const token = getFromStorage("token");
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          "x-auth-token": token
+        }
+      };
+      const body = JSON.stringify(taskData);
+      const res = await axios.post("/api/tasks", body, config);
+      const { data } = await res;
+      return data;
+    } catch (err) {
+      if (err.response) {
+        const errors = await err.response.data;
+        console.log(errors);
+        throw errors;
+      } else {
+        throw err;
+      }
+    }
+  }
 }
